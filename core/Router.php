f7b66e2f60b851d3df2aa65359dde1e82c4cc0f6
octavio.godoy <octavio.godoy@segic.usach.cl>
2021-05-11 14:40:56 -0400

    Se crea request

--- core/Router.php
+++ core/Router.php
@@ -3,16 +3,27 @@ namespace app\core;
 
 class Router
 {
+    public Request $request;
     protected array $routes=[];
+
+    public function __contruct(\app\core\Request $request ){
+        $this->request =$request;
+    }
 
     public function get($path, $callback){
         $this->routes['get'][$path]= $callback;
     }
 
     public function resolve(){
-        echo '<pre>';
-        var_dump($_SERVER);
-        echo '/<pre>';
-        exit;
+        // echo '<pre>';
+        // var_dump($_SERVER);
+        // echo '/<pre>';
+        // exit;
+
+        $path = $this->request->getPath();
+        $method = $this->request->getMethod();
+
+        var_dump($path);
+        var_dump($method);
     }
 }
